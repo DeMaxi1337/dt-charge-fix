f3eedc07fff8072b613a3554f69709a050fb0d16
Maxim <81583022+DeMaxi1337@users.noreply.github.com>
2025-12-16 14:44:14 +0300

    Update dt-charge-fix.lua

--- dt-charge-fix.lua
+++ dt-charge-fix.lua
@@ -5,7 +5,6 @@ local mem_buffer = ffi.new("char[?]", patch_size)
 local mem_original = ffi.new("char[?]", patch_size)
 local mem_address = ffi.cast("char*", 0x433AC04B)
 
--- Делаем копию оригинала и готовим патч
 ffi.copy(mem_original, mem_address, patch_size)
 ffi.copy(mem_buffer, mem_original, patch_size)
 
@@ -14,10 +13,8 @@ for i = 0, 23 do
 end
 mem_buffer[24] = 0xE9
 
--- Элемент интерфейса
 local ui_switch = ui.new_checkbox("RAGE", "Other", "Unsafe Charge")
 
--- Обработчик
 ui.set_callback(ui_switch, function()
     if ui.get(ui_switch) then
         ffi.copy(mem_address, mem_buffer, patch_size)
@@ -26,9 +23,9 @@ ui.set_callback(ui_switch, function()
     end
 end)
 
--- Восстановление при выгрузке
 client.set_event_callback("shutdown", function()
     if ui.get(ui_switch) then
         ffi.copy(mem_address, mem_original, patch_size)
     end
+
 end)
